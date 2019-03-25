15faa97e21195491cdd1f4b0b87e84e1da2233c8
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-03-25 14:37:33 -0700

    Add koala configurations

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,8 +1,6 @@
 Koala.configure do |config|
-  config.access_token = MY_TOKEN
-  config.app_access_token = MY_APP_ACCESS_TOKEN
-  config.app_id = MY_APP_ID
-  config.app_secret = MY_APP_SECRET
-  # See Koala::Configuration for more options, including details on how to send requests through
-  # your own proxy servers.
+
+  config.app_id = Rails.application.credentials.facebook[:facebook_id]
+  config.app_secret = Rails.application.credentials.facebook[:facebook_secret_key]
+
 end
